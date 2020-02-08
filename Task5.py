60c426f98b6b367b7409d464b08f9f849950c6bb
KirillMikh <60451069+KirillMikh@users.noreply.github.com>
2020-02-09 00:00:27 +0200

    Update Task5.py

--- Task5.py
+++ Task5.py
@@ -1,4 +1,11 @@
 import random
+allowed_symbols = []
+for symbol in range(ord("0"), ord("9") + 1):
+    allowed_symbols.append(chr(symbol))
+for symbol in range(ord("a"), ord("z") + 1):
+    allowed_symbols.append(chr(symbol))
+for symbol in range(ord("A"), ord("Z") + 1):
+    allowed_symbols.append(chr(symbol))
 str1 = "1"
 while str1.isdecimal():
     str1 = input("Введите длину пароля")
@@ -7,13 +14,6 @@ while str1.isdecimal():
     if int(str1) == 0:
         exit(0)
     password = ""
-    allowed_symbols = []
-    for symbol in range(ord("0"), ord("9") + 1):
-        allowed_symbols.append(chr(symbol))
-    for symbol in range(ord("a"), ord("z") + 1):
-        allowed_symbols.append(chr(symbol))
-    for symbol in range(ord("A"), ord("Z") + 1):
-        allowed_symbols.append(chr(symbol))
     for symbol in range(int(str1)):
         password += random.choice(allowed_symbols)
     print("Ваш пароль: ", password)

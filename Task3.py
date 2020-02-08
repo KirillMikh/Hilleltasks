8adb014e38e33dc7d220285c1fed6f51af58eb45
KirillMikh <60451069+KirillMikh@users.noreply.github.com>
2020-02-08 23:59:47 +0200

    Update Task3.py

--- Task3.py
+++ Task3.py
@@ -1,9 +1,9 @@
 str1 = input("Введите строку ")
 list1 = list(str1)
-list2 = list1
-list2.reverse()
-if list1 == list2:
-    print(" слово является палиндромом")
-else:
-    print(" слово не является палиндромом")
+for i in range(len(list1)):
+    if list1[i] != list1[len(list1)-i-1]:
+        print(" слово не является палиндромом")
+        break
+    elif i == len(list1)-1:
+        print(" слово  является палиндромом")
 

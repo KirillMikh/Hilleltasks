aac34539c9f5d828ae5c1c2b2cfb4181cd5dffad
KirillMikh <60451069+KirillMikh@users.noreply.github.com>
2020-02-15 16:48:08 +0200

    Update Task1.py

--- Task1.py
+++ Task1.py
@@ -7,12 +7,7 @@ list1 = [
 	[-3, -2, -1, 0, 1, 2, 3, 4, 5, 6],
 	[100, 200, 300, 400, 500, 600]
 ]
-i = 0
+list_sums = []
 for element in list1:
-	count = 0
-	for number in element:
-		if number % 2 == 1:
-			count += number
-	i += 1
-	print("сумма нечетных чисел в строке номер " + str(i) + " равняется "+ str(count))
-
+	list_sums.append((sum(i for i in element if i % 2 == 1)))
+print(*list_sums,sep="\n")

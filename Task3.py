84de0a3879ff3a476f6fe1e03a18d95411081e37
KirillMikh <60451069+KirillMikh@users.noreply.github.com>
2020-02-15 16:48:35 +0200

    Update Task3.py

--- Task3.py
+++ Task3.py
@@ -4,14 +4,15 @@ dict1 = dict()
 common_type_number = 0
 common_type = ""
 for element in list1:
-    if str(type(element)) not in set1:
-        set1.add(str(type(element)))
-        dict1.update({str(type(element)) : 1})
+    tp = str(type(element))
+    if tp not in set1:
+        set1.add(tp)
+        dict1[tp] = 1
     else:
-        dict1.update({str(type(element)): dict1.get(str(type(element)))+1})
-        if common_type_number <= dict1.get(str(type (element))):
-            common_type_number = dict1.get(str(type (element)))
-            common_type = str(type(element))
+        dict1[tp] = dict1.get(tp, 0) + 1
+        if common_type_number <= dict1.get(tp):
+            common_type_number = dict1.get(tp)
+            common_type = tp
 print (dict1)
 print("Самый часто встречаемы объект " + common_type)
 for element in list1:

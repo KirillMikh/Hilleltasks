05b2a7873a51f5cfe087a721d8575ae4685288e5
KirillMikh <60451069+KirillMikh@users.noreply.github.com>
2020-02-22 10:49:19 +0200

    Update NumberDecorator.py

--- NumberDecorator.py
+++ NumberDecorator.py
@@ -3,12 +3,13 @@ def decoratot_number(func):
         list2=[]
         for i in listq:
             list2.append("+91 "+ str(i[len(i)-10:len(i)-5])+" "+ str(i[len(i)-5: ]))
-        return list2
+        return func(list2)
     return wrapper
 
 @decoratot_number
 def sort_function(list1):
-    return list1.sort()
+    list1.sort()
+    return list1
 
 
 
@@ -17,6 +18,6 @@ def input_number():
     list1 = []
     for i in range(str1):
         list1.append(input("Введите " + str(i + 1) + " номер"))
-    print(*sort_function(list1),sep="\n")
+    print(sort_function(list1), sep="\n")
 
 input_number()

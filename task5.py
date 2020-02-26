8507297ab37b4ee1670054d407f3f468d99f90c5
KirillMikh <60451069+KirillMikh@users.noreply.github.com>
2020-02-26 22:07:08 +0200

    Update task5.py

--- task5.py
+++ task5.py
@@ -1,9 +1,20 @@
 import string
-alpha = string.ascii_lowercase
 
-n = int(input())
-L = []
-for i in range(n):
-    s = "-".join(alpha[i:n])
-    L.append((s[::-1]+s[1:]).center(4*n-3, "-"))
-print('\n'.join(L[:0:-1]+L))
+
+def rangoli_maker(size:int):
+    all_letters = string.ascii_lowercase
+    a = size
+    letters = all_letters[0:a]
+    str1 = list()
+    line_length = 4*a-3
+    for i in range(a):
+        temp_str = "-".join(reversed(letters[i:a]))
+        if len(letters[i+1:a]) < 1:
+            temp_str = temp_str+"-".join(letters[i+1:a])
+        else:
+            temp_str = temp_str + "-" + "-".join(letters[i + 1:a])
+        str1.append(temp_str.center(line_length, "-"))
+
+    print(*reversed(str1[1::]), *str1, sep="\n")
+
+rangoli_maker(5)

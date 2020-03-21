825f632d6a7eac16866426349ffa6a4d4ae10e0e
KirillMikh <60451069+KirillMikh@users.noreply.github.com>
2020-03-21 16:21:06 +0200

    Update 2.py

--- 2.py
+++ 2.py
@@ -1,40 +1,29 @@
-
-def exception_handler(func,*args):
+def decorator_maker(*args):
     errors_tuple = args
 
-    def wrapper(*args1):
-        try:
-            result = func(*args1)
-        except errors_tuple:
-            return print("Возникла ошибка но была подавлена")
-        return result
-    return wrapper
+    def exception_handler(func):
+        def wrapper(*args1):
+            try:
+                result = func(*args1)
+            except errors_tuple:
+                return print("Возникла ошибка но была подавлена")
+            return result
+        return wrapper
+    return  exception_handler
 
 
+@decorator_maker(ZeroDivisionError, TypeError)
 def f1(a):
     if a == 1:
         print(2131 + "string")
     else:
         print(1/0)
 
-
+@decorator_maker(IndexError)
 def f2():
     print([][5])
 
-
-def f3():
-    a = open("sdsdsdsq12a79852142321212","r")
-    a.close()
-
-
-f1 = exception_handler(f1, ZeroDivisionError, TypeError)
 f1(1)
 f1(2)
-
-f2 = exception_handler(f2, IndexError)
 f2()
 
-
-#Здесь возникнет ошибка, потому что тип ошибки которую надо подавить-FileNotFound
-# f3= exception_handler(f3, TypeError)
-# f3()

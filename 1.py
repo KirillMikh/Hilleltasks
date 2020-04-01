98ce4eae613e3a085866ab5e33420f81696f0fe3
KirillMikh <60451069+KirillMikh@users.noreply.github.com>
2020-04-01 18:44:50 +0300

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,13 +1,14 @@
 class MyRange:
-    def __init__(self, stop, start=0, step=1):
-        if start == 0:
-            self.start = start
-            self.stop = stop
-            self.step = step
+    def __init__(self, *args):
+        tuple1 = tuple(args)
+        self.start = 0
+        self.step = 1
+        if len(tuple1) == 1:
+            self.stop = tuple1[0]
+        elif len(tuple1) == 2:
+            self.start, self.stop = tuple1
         else:
-            self.start = stop
-            self.stop = start
-            self.step = step
+            self.start, self.stop, self.step = tuple1
 
     def __iter__(self):
         return self
@@ -24,18 +25,25 @@ class MyRange:
 for i in MyRange(20):
     print(i, end=' ')
 print("\n")
-for i in MyRange(4, 20):
+for i in MyRange(7, 20):
     print(i, end=' ')
 print("\n")
 
-for i in MyRange(5,20,5):
+for i in MyRange(5, 20, 5):
     print(i, end=' ')
 print("\n")
 
 
-def my_generator(stop, start=0, step=1):
-    if start != 0:
-        start, stop = stop, start
+def my_generator(*args):
+    tuple1 = tuple(args)
+    start = 0
+    step = 1
+    if len(tuple1) == 1:
+        stop = tuple1[0]
+    elif len(tuple1) == 2:
+        start, stop = tuple1
+    else:
+        start, stop, step = tuple1
     while start < stop:
         yield start
         start += step
@@ -44,10 +52,10 @@ def my_generator(stop, start=0, step=1):
 for i in my_generator(20):
     print(i, end=" ")
 print("\n")
-for i in my_generator(4, 20):
+for i in my_generator(7, 20):
     print(i, end=" ")
 print("\n")
 
-for i in my_generator(5,20,5):
+for i in my_generator(5, 20, 5):
     print(i, end=" ")
 print("\n")
